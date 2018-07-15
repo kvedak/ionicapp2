3eed216abd05cce1648fb092bbd8dca06c0efeb5
Kiran Vedak <kiran@vedaks.com>
2018-07-15 11:47:52 -0500

    added Ayut's changes and fixed a bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,23 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { TutorialsPage } from '../pages/tutorials/tutorials';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import { TabsPage } from '../pages/tabs/tabs'
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
+    AboutPage,
+    TutorialsPage,
     TabsPage
+
+
   ],
   imports: [
     BrowserModule,
@@ -26,10 +29,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
+    AboutPage,
+    TutorialsPage,
     TabsPage
+
   ],
   providers: [
     StatusBar,
@@ -37,4 +41,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
+
+
 export class AppModule {}
